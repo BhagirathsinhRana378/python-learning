096adbd6215ac730622435ca538f8af9767dfed5
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-16 12:48:23 +0530

    add for loop examples with print statements

---
+++ forloop5.py
@@ -0,0 +1,9 @@
+for number in range(3):
+    print("loop is running", number+1, (number+1)* '.' )
+# bit complex
+print("loop is finished")
+for number in range(1,5):
+    print("loop is running", number, (number)* '.' )
+# bit complex
+
+input("press enter to exit")
