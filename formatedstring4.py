bbd9c75a05898a6f25ba3f230674334136a224b9
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-16 12:17:48 +0530

    strings learning

---
+++ formatedstring4.py
@@ -0,0 +1,12 @@
+first = 'Bhagi'
+last = 'batman'
+
+message = first + '[' + last + '] is the batman'
+# this is too complex to understand
+# so we have formated string
+
+msg = f'{first} [{last}] is the batman'
+#this one is easy to understand and write
+
+print(message)
+print(msg)
---
+++ strings3.py
@@ -0,0 +1,11 @@
+multilinestr = '''
+this is a multiline string
+it is written in mulipal lines
+'''
+print(multilinestr[0:23])
+print(multilinestr[9])
+print(multilinestr[8:-3])
+ 
+another = multilinestr[:]
+
+print(another)
