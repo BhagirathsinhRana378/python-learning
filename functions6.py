175d46b8a3ce2662ea4e265ce1479f47fe516588
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-16 16:42:39 +0530

    feat: add practice scripts for functions, emoji conversion, and exception handling

---
+++ functions6.py
@@ -0,0 +1,12 @@
+# def greet():
+#     print("hii")
+#     print("meloc")
+
+# greet()
+
+
+# def greet(firstname, lastname):
+#     print(f"hii{firstname} {lastname}")
+#     print("meloc")
+
+# greet("bhagi","batman")
