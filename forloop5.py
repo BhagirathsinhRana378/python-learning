8b1ab9f906b5bd0146aab04582a7c237a2c254d5
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-16 16:01:01 +0530

    refactor for loop examples and enhance even number counting logic

--- forloop5.py
+++ forloop5.py
@@ -1,9 +1,15 @@
-for number in range(3):
-    print("loop is running", number+1, (number+1)* '.' )
-# bit complex
-print("loop is finished")
-for number in range(1,5):
-    print("loop is running", number, (number)* '.' )
-# bit complex
+# for number in range(3):
+#     print("loop is running", number+1, (number+1)* '.' )
+# # bit complex
+# print("loop is finished")
+# for number in range(1,5):
+#     print("loop is running", number, (number)* '.' )
+# # bit complex
 
-input("press enter to exit")
+# input("press enter to exit")
+count = 0
+for number in range(1,10):
+    if number %2 ==0:
+        count += 1
+        print(number)
+print(f"we have {count} even numbers ")
