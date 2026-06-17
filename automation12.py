47b7c8410e7cd986fd2134730270911e98a6f2d0
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-17 13:18:22 +0530

    `Added import statement for openpyxl chart module`

--- automation12.py
+++ automation12.py
@@ -1,4 +1,5 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 # used to load the workbook
 wb = xl.load_workbook('transactions-forpyautomation.xlsx')
@@ -19,5 +20,15 @@ for row in range(2,sheet.max_row +1):
     correected_price_cell = sheet.cell(row,4)
     correected_price_cell.value = corrected_price
     #print(cell.value) # we got the price
+values = Reference(sheet,
+        min_row=2,
+        max_row=sheet.max_row,
+        min_col=4,
+        max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+
+sheet.add_chart(chart, 'e2')
 
 wb.save('transactions-forpyautomation2(copy).xlsx')
