3229ebe2393d2ef61a8064230a78e96d8b037459
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-17 13:10:14 +0530

    feat: implement automated price adjustment logic and save results to a new workbook

--- automation12.py
+++ automation12.py
@@ -12,3 +12,12 @@ cell = sheet.cell(1,1)
 # better method to get the cell(row,column)
 
 print(cell.value)
+
+for row in range(2,sheet.max_row +1):
+    cell = sheet.cell(row,3)
+    corrected_price = cell.value *0.7
+    correected_price_cell = sheet.cell(row,4)
+    correected_price_cell.value = corrected_price
+    #print(cell.value) # we got the price
+
+wb.save('transactions-forpyautomation2(copy).xlsx')
