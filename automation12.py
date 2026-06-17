7a01cf5553e9dda0659fda7a66c24834e371db97
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-17 13:21:56 +0530

    refactor: encapsulate workbook processing into a reusable function and remove temporary copy file

--- automation12.py
+++ automation12.py
@@ -1,34 +1,40 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-# used to load the workbook
-wb = xl.load_workbook('transactions-forpyautomation.xlsx')
 
-sheet = wb['Sheet1']
-# accessed the sheet 1 for the excel file
+def process_workbook(filename):
 
-# cell = sheet['a1']
-# we are definning the cell 
-cell = sheet.cell(1,1) 
-# better method to get the cell(row,column)
+    # used to load the workbook
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+    # accessed the sheet 1 for the excel file
 
-print(cell.value)
+    # cell = sheet['a1']
+    # we are definning the cell 
+    cell = sheet.cell(1,1) 
+    # better method to get the cell(row,column)
 
-for row in range(2,sheet.max_row +1):
-    cell = sheet.cell(row,3)
-    corrected_price = cell.value *0.7
-    correected_price_cell = sheet.cell(row,4)
-    correected_price_cell.value = corrected_price
-    #print(cell.value) # we got the price
-values = Reference(sheet,
-        min_row=2,
-        max_row=sheet.max_row,
-        min_col=4,
-        max_col=4)
+    print(cell.value)
 
-chart = BarChart()
-chart.add_data(values)
+    for row in range(2,sheet.max_row +1):
+        cell = sheet.cell(row,3)
+        corrected_price = cell.value *0.7
+        correected_price_cell = sheet.cell(row,4)
+        correected_price_cell.value = corrected_price
+        #print(cell.value) # we got the price
 
-sheet.add_chart(chart, 'e2')
 
-wb.save('transactions-forpyautomation2(copy).xlsx')
+    values = Reference(sheet,
+            min_row=2,
+            max_row=sheet.max_row,
+            min_col=4,
+            max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
+
+process_workbook('transactions-forpyautomation.xlsx')
