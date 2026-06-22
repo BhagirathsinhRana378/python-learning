355f28d3c92284dffc4f92ddcd480866446c15a2
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-22 16:41:06 +0530

    feat: add examples for basic arithmetic and string multiplier functions

---
+++ Basics/pyfunction13.py
@@ -0,0 +1,36 @@
+#Problem: Write a function to calculate and return the square of a number.
+# def squarfun(n):
+#     print(f'The square for the {n} is {n*n}')
+
+# squarfun(9)
+# squarfun(8)
+# squarfun(7)
+# squarfun(5)
+# squarfun(4)
+#--------------------1st function part over---------------
+#----------------------2nd function part--------------------
+#Create a function that takes two numbers as parameters and returns their sum.\
+
+
+# def sumfun(a,b):
+#     print(f'The sum of {a} and {b} is {a+b}')
+
+# sumfun(2,4)
+
+#----------------------2nd function part over--------------------
+
+#----------------------3rd function part--------------------
+#: Write a function multiply that multiplies two numbers, but can also accept and multiply strings.
+
+# def multiply(a,b):
+#     print(f'the multiplication of {a} X {b} is :{a*b}')
+
+
+
+# multiply('a',4)
+# multiply(5,'bhagi')
+ 
+# multiply = lambda a,b: a*b
+# print(multiply('a',4))
+#-----------------------3rd function over-----------------------
+
