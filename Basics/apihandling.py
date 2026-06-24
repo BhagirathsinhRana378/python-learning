1d8f27e985e711a953f8d3f91abb10ad0bd83772
BhagirathsinhRana378 <bhagirathbhai378@gmail.com>
2026-06-24 13:05:11 +0530

    fix: correct API endpoint and enhance user data retrieval in fetch function

--- Basics/apihandling.py
+++ Basics/apihandling.py
@@ -1,10 +1,24 @@
 import requests
-
 def fectch_random_user_freeapi():
-    url = "https://api.freeapi.app/api/v1/public/randomusers/user/Fandom"
+    url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
     response = requests.get(url)
-    return response.json()
+    data =  response.json()
 
     if data["success"] and "data" in data:
-        data["data"]
-    
+        user_data = data["data"]
+        username = user_data["login"]["username"]
+        country = user_data["location"]["country"]
+        return username, country
+    else:
+        raise Exception(" failed to fetch the data")
+
+
+def main():
+    try:
+        username, country = fectch_random_user_freeapi()
+        print(f"Username:{username} \n Country:{country}")
+    except Exception as e:
+        print(str(e))
+
+if __name__ == "__main__":
+    main()
